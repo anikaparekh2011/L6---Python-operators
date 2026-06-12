349a1d1101c3f591c479db62f5315892beedf4aa
anikaparekh2011 <anikaparekh2011@gmail.com>
2026-06-12 21:04:25 +0100

    Add files via upload

---
+++ alphabetchecking.py
@@ -0,0 +1,11 @@
+
+
+
+alphabet2 = "abcdefghijklmnopqrstuvwxyz"
+
+letter = str(input("Enter letter here: "))
+
+if letter in alphabet2:
+    print("your letter exists in alphabet")
+else:
+    print("nope does not exist in alphabet")
