426ec46226a059870d070f54244bd14aacb83897
anikaparekh2011 <anikaparekh2011@gmail.com>
2026-06-12 20:21:13 +0100

    Add files via upload

---
+++ logicalnotoperator.py
@@ -0,0 +1,15 @@
+a = 20 
+b = 20
+c = 30
+
+if not ((a==20)==(c==40)):
+    print("true")
+else:
+    print("false")
+
+n = int(input("Enter your number: "))
+
+if not (n%2==0):
+    print("number is odd")
+else:
+    print("number is even")
